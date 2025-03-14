2a9fa09a7bac7739c2868c7af544396700f4b090
Ngô Trần Đức Thái <119216607+thaintd502@users.noreply.github.com>
2025-03-14 23:57:54 +0700

    Update CORSConfig.java

--- src/main/java/com/ecom2/config/CORSConfig.java
+++ src/main/java/com/ecom2/config/CORSConfig.java
@@ -17,9 +17,13 @@ public class CORSConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://34.92.164.246:3000"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedOrigins(Arrays.asList(
+            "http://localhost:3000", 
+            "https://radiant-starburst-5f7d9b.netlify.app", 
+            "http://34.92.164.246:3000"
+        ));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
